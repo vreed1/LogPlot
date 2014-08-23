2c5311879db4cb6982420978557d43068f0b1d9b
Victoria Reed <vreed1@lsu.edu>
2014-08-23 09:49:17 -0500

    restructuring of view/controller so that controller now creates view

--- logplot/logPlotController.py
+++ logplot/logPlotController.py
@@ -1,6 +1,9 @@
+import sys
+import logPlotView as lpv
 from utils import *
 from dbTools import *
 from grapher import *
+from PyQt4.QtGui import QApplication
 
 class Controller:
 
@@ -8,45 +11,89 @@ class Controller:
         self.currentFile = ""
         self.tempRecords = []
         self.graph = Grapher()
+        self.view = lpv.LogPlot()
+        self.view.show()
+        self.ConnectViewEvents()
+
+    def ConnectViewEvents(self):
+        #Connect buttons from Upload tab
+        self.view.ui.FileBrowseButton.clicked.connect(self.FileBrowse)
+        self.view.ui.RecordButton.clicked.connect(self.Record)
+        self.view.ui.PlotExcelButton.clicked.connect(self.PlotExcel)
+        self.view.ui.previewSummary.toggled.connect(self.PreviewSummary)
+        self.view.ui.previewAll.toggled.connect(self.PreviewAll)
+
+        #Connect buttons from Plot tab
+        #self.view.ui.plotButton.clicked.connect(self.PlotFromDatabase)        
+
+#-----------------Event Handlers---------------------   
+    def FileBrowse(self):
+        path = self.view.FileBrowseDialog()
+        if not self.FileNameValid(path, '.csv'): return
+        self.view.ui.filePath.setText(path)
+        self.view.ui.previewSummary.setChecked(True)
         
-    def readRecords(self, filename):
-        #Store name of file
-        self.currentFile = filename
-        #Read in records
-        self.tempRecords = Utils.readRecordsFromFile(filename)
-        
-    def RecordDataFromFile(self, filename):
+    def Record(self):
+        filename = self.view.ui.filePath.text()
         if self.currentFile != filename:
-            self.readRecords(filename)
+            self.ReadRecords(filename)
         #Get daily summary
         summaryRecords = Utils.makeSummary(self.tempRecords)
         #Add to daily summary table in database
         DailySummary.insert(summaryRecords)
 
-    def PlotSummaryPreview(self, filename):
-        if self.currentFile != filename:
-            self.readRecords(filename)
-        #Create summary
-        summaryRecords = Utils.makeSummary(self.tempRecords)
-        #figure should be reference to MatPlot figure
-        return self.graph.plotSummaryPreview(summaryRecords)
+    def PreviewSummary(self):
+        self.PlotPreview(self.view.ui.filePath.text())
 
-    def PlotDetailedPreview(self, filename):
-        if self.currentFile != filename:
-            self.readRecords(filename)
-        #figure should be reference to MatPlot figure
-        return self.graph.plotTempPreview(self.tempRecords)
-
-    def PlotTempsExcel(self, filename):
-        if not self.tempRecords:
-            #TODO: error, no records to plot
-            return
-        return self.graph.plotTempsExcel(filename, self.tempRecords)
-
-    def PlotSummaryExcel(self, filename):
-        if not self.tempRecords:
-            #TODO: error, no records to plot
-            return
-        summaryRecords = Utils.makeSummary(self.tempRecords)
-        return self.graph.plotTempSummaryExcel(filename, summaryRecords)
+    def PreviewAll(self):
+        self.PlotPreview(self.view.ui.filePath.text(), summary = False)        
         
+    def PlotExcel(self):
+        #Get directory of save location
+        savePath = self.view.SaveDialog("untitled.xlsx", "Excel files (*xlsx)")
+        if not self.FileNameValid(savePath, '.xlsx'): return
+
+        if not self.tempRecords:
+            #TODO: error, no records to plot
+            return
+
+        if self.view.ui.previewAll.isChecked():
+            self.graph.plotTempsExcel(savePath, self.tempRecords)
+        else:
+            summaryRecords = Utils.makeSummary(self.tempRecords)
+            self.graph.plotTempSummaryExcel(savePath, summaryRecords)
+
+#-----------------Helper Methods--------------------- 
+    
+    def PlotPreview(self, filename, summary = True):
+        if self.currentFile != filename:
+            self.ReadRecords(filename)
+        #Get appropriate records and plot
+        figure = None
+        if summary:
+            records = Utils.makeSummary(self.tempRecords)
+            figure = self.graph.plotSummaryPreview(records)
+        else:
+            figure = self.graph.plotTempPreview(self.tempRecords)       
+        #Update preview canvas in view
+        self.view.UpdatePreviewCanvas(figure)
+
+    def FileNameValid(self, filename, ending):
+        if not filename or not filename.endswith(ending):
+            #TODO: error dialog, must provide valid filename
+            return False
+        else: return True
+
+    def ReadRecords(self, filename):
+        #Store name of file
+        self.currentFile = filename
+        #Read in records
+        self.tempRecords = Utils.readRecordsFromFile(filename)
+
+
+
+        
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    controller = Controller()
+    sys.exit(app.exec_())
--- logplot/logPlotView.py
+++ logplot/logPlotView.py
@@ -1,5 +1,6 @@
 import sys
 import matplotlib.pyplot as plt
+from datetime import datetime
 from PyQt4.QtGui import QApplication, QMainWindow, QFileDialog, QVBoxLayout
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
@@ -9,7 +10,7 @@ from Interface import Ui_LogPlot
 class LogPlot(QMainWindow, Ui_LogPlot):
 
     def __init__(self):
-        self.controller = Controller()
+        #self.controller = Controller()
         QMainWindow.__init__(self)
 
         #Set up the user interface from Designer
@@ -18,13 +19,6 @@ class LogPlot(QMainWindow, Ui_LogPlot):
 
         #Set up plots
         self.CreateGraphCanvases()
-        
-        #Connect buttons
-        self.ui.FileBrowseButton.clicked.connect(self.FileBrowse)
-        self.ui.RecordButton.clicked.connect(self.Record)
-        self.ui.PlotExcelButton.clicked.connect(self.PlotExcel)
-        self.ui.previewSummary.toggled.connect(self.PreviewSummary)
-        self.ui.previewAll.toggled.connect(self.PreviewAll)
 
     def CreateGraphCanvases(self):
         #Create empty plot
@@ -50,66 +44,23 @@ class LogPlot(QMainWindow, Ui_LogPlot):
         plot.addWidget(canvas)
         plot.addWidget(toolbar)
         return plot
-        
-    def FileBrowse(self):
-        self.ui.filePath.setText(QFileDialog.getOpenFileName())
-        if not self.FileNameValid(self.ui.filePath.text()): return
-        #By default, plot preview of summary when a file is opened
-        self.ui.previewSummary.setChecked(True)
-        self.PreviewSummary()
-        
-    def Record(self):
-        records = []
-        filename = self.ui.filePath.text()
-        if not self.FileNameValid(filename): return
-        self.controller.RecordDataFromFile(filename)
 
-    def FileNameValid(self, filename):
-        if not filename or not filename.endswith('.csv'):
-            #TODO: error dialog, must provide valid filename
-            return False
-        else: return True
-
-    def PreviewSummary(self):
+    def UpdatePreviewCanvas(self, figure):
         #get size of current figure and then clear
         figSize = self.previewCanvas.figure.get_size_inches()
         self.previewCanvas.figure.clf()
         #get new figure and adjust size
-        self.previewCanvas.figure = self.controller.PlotSummaryPreview(self.ui.filePath.text())
+        self.previewCanvas.figure = figure
         self.previewCanvas.figure.set_size_inches(figSize)
         #re-draw canvas
         self.previewCanvas.draw()
 
-    def PreviewAll(self):
-        #get size of current figure and then clear
-        figSize = self.previewCanvas.figure.get_size_inches()
-        self.previewCanvas.figure.clf()
-        #get new figure and adjust size
-        self.previewCanvas.figure = self.controller.PlotDetailedPreview(self.ui.filePath.text())
-        self.previewCanvas.figure.set_size_inches(figSize)
-        #re-draw canvas
-        self.previewCanvas.draw()
+    def FileBrowseDialog(self):
+        return QFileDialog.getOpenFileName()
 
-    def PlotExcel(self):
-        #Get directory of save location
-        savePath = str(QFileDialog.getSaveFileName(self,
-                                                   "Choose save location",
-                                                   "untitled.xlsx",
-                                                   "Excel files (*xlsx)"))
-        if not savePath or not savePath.endswith('.xlsx'):
-            #TODO: handle bad save path
-            return
+    def SaveDialog(self, defaultName, extensionFilter):
+        return str(QFileDialog.getSaveFileName(self,"Choose save location",
+                                               defaultName,extensionFilter))
         
-        #Plotting all records or just summary?
-        if self.ui.previewAll.isChecked(): self.controller.PlotTempsExcel(savePath)
-        elif self.ui.previewSummary.isChecked(): self.controller.PlotSummaryExcel(savePath)
-        else: return
-        
-        
-        
-        
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    logPlot = LogPlot()
-    logPlot.show()
-    sys.exit(app.exec_())
+
+    
